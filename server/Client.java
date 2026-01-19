0999d9ffbaeca52da52227e392e3ceb1560118ee
Dhrimanti <dhritimancg@gmail.com>
2026-01-19 12:37:36 +0530

    Please help me

---
+++ server/Client.java
@@ -0,0 +1,19 @@
+package server;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+public class Client {
+
+
+    public void run() throws Exception{
+        int port =3000;
+        InetAddress address=InetAddress.getByName("localhost");
+        Socket socket=new Socket(address,port);
+        PrintWriter toSocket=new PrintWriter(socket.getOutputStream())
+    }
+    public static void main(String[] args) {
+
+        
+    }
+    
+}
