9a22270f3a3ffa7c6e9d335425e97365d73530f5
Dhrimanti <dhritimancg@gmail.com>
2026-01-19 17:10:51 +0530

    Please help me

--- multithread/Server.java
+++ multithread/Server.java
@@ -1,7 +1,43 @@
 package multithread;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.function.Consumer;
+import java.io.PrintWriter;
+import java.lang.Thread;
 public class Server {
+
+
+    public Consumer<Socket> getConsumer(){
+        return (clientSocket)->{
+            try{
+                PrintWriter toClient=new PrintWriter(clientSocket.getOutputStream());
+                toClient.println("Hello from this new server");
+                toClient.close();
+                clientSocket.close();
+
+            }catch(Exception e){
+                
+            }
+        }
+
+    }
     public static void main(String[] args) {
+        int port =3000;
+        Server server=new Server();
+        try{
+            ServerSocket serverSocket=new ServerSocket(port);
+            serverSocket.setSoTimeout(70000);
+            System.out.println("Server is running on the port"+port);
+            while(true){
+                Socket clientSocket=serverSocket.accept();
+                Thread thread=new Thread(()->server.getConsumer().accept(clientSocket));
+                thread.start();
+
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         
     }
     
